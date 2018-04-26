c7f71c3434ebf7fc9449fb54f1cf7a02a50e82a0
nano <nano.suarez@orderbird.com>
2018-04-26 09:49:00 +0200

    initial commit

---
+++ codeChallenge/CustomCell.h
@@ -0,0 +1,16 @@
+//
+//  CustomCell.h
+//  codeChallenge
+//
+//  Created by Nano Suarez on 18/04/2018.
+//  Copyright © 2018 Fernando Suárez. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CustomCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageCell;
+@property (weak, nonatomic) IBOutlet UILabel *imageTitleCell;
+@property (weak, nonatomic) IBOutlet UILabel *imageSubtitleCell;
+
+@end
---
+++ codeChallenge/CustomCell.m
@@ -0,0 +1,22 @@
+//
+//  CustomCell.m
+//  codeChallenge
+//
+//  Created by Nano Suarez on 18/04/2018.
+//  Copyright © 2018 Fernando Suárez. All rights reserved.
+//
+
+#import "CustomCell.h"
+
+@implementation CustomCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+    
+    
+    self.imageTitleCell.translatesAutoresizingMaskIntoConstraints = YES;
+}
+
+
+@end
